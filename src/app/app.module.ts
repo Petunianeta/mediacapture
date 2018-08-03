dc69e46715003d4d992aedc854cb608133359fce
Petunia Neta <Petunianeta@gmail.com>
2018-08-03 09:00:04 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions   } from '@ionic-native/media-capture';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -24,6 +24,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    MediaCapture,
+    
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions, CaptureAudioOptions,CaptureVideoOptions } from '@ionic-native/media-capture';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private mediaCapture: MediaCapture) {
 
   }
+onImage(){
+  let options: CaptureImageOptions = { limit: 3 };
+this.mediaCapture.captureImage(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+
+}
+onAudio(){
+  let options: CaptureAudioOptions = { limit: 3 };
+this.mediaCapture.captureAudio(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+}
+onVedio(){
+  let options: CaptureVideoOptions = { limit: 3 };
+this.mediaCapture.captureVideo(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+}
 
 }
